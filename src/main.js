8a78bd46195dfaae673a784908962ed92b25d810
zhuzedan <1031155817@qq.com>
2023-01-05 12:27:05 +0800

    自定义样式

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.scss'
 
 Vue.use(ElementUI)
 

995897fdcde7e42debbe3e826b3011825a132ef9
zhuzedan <1031155817@qq.com>
2023-01-05 16:34:38 +0800

    同上

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app-header">
+    <i class="el-icon-s-fold" @click="toggleCollapse"></i>
+
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        <el-avatar shape="square" :size="40" src="https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png"></el-avatar>
+        <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>用户信息</el-dropdown-item>
+        <el-dropdown-item>退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  props: {
+    isCollapse: {
+      type: Boolean,
+      default: false
+    },
+    toggleCollapse: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped lang='scss'>
+.app-header {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  .el-breadcrumb {
+    margin-left: 10px;
+  }
+  .el-dropdown {
+    margin-left: auto;
+    .el-dropdown-link {
+      display: flex;
+      align-items: center;
+    }
+  }
+}
+</style>
